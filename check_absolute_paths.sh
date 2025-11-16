83944e86e1b339b711499b7cded236e0b0bde61f
stertooy <5571903+stertooy@users.noreply.github.com>
2025-11-16 21:10:23 +0100

    Update check_absolute_paths.sh

--- check_absolute_paths.sh
+++ check_absolute_paths.sh
@@ -2,6 +2,8 @@
 set -e
 set -o pipefail
 
-if fgrep -q -r '<a href="/' *.htm* || fgrep -q -r '<a href="file://' *.htm* ; then
+shopt -s globstar
+
+if grep -rlE '<a href="(file:/)?/' **/*.htm* ; then
   exit 1
 fi
